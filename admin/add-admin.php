20d21be3b681e6795392584ceab777ca5402daab
SaucyDude <flurrychef@gmail.com>
2022-04-08 02:03:07 +0800

    first commit

---
+++ admin/add-admin.php
@@ -0,0 +1,26 @@
+<?php include('partials/menu.php')?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Add Admin</h1>
+
+        <form action="" method="POST">
+            <table>
+                <tr>
+                    <td>Full Name:</td>
+                    <td>
+                        <input type="text" name="full_name" placeholder="Enter your full name">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Username</td>
+                    <td>
+                        <input type="text" name="user_name" placeholder="Enter your user name">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</div>
+
+<?php include('partials/footer.php')?>
---
+++ admin/manage-admin.php
@@ -0,0 +1,55 @@
+
+<?php include_once('partials/menu.php'); ?>
+
+<div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Admin</h1>
+            <a href="add-admin.php" class="btn-primary">Add Admin</a>
+            <table class="tbl-full">
+                <tr class="tbl-title">
+                    <th>S.N</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Baby Em</td>
+                    <td>emBie</td>
+                    <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Baby Em</td>
+                    <td>emBie</td>
+                    <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Baby Em</td>
+                    <td>emBie</td>
+                    <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Baby Em</td>
+                    <td>emBie</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    
+ <?php include('partials/footer.php') ?>   
